7b90456c2842f4fbf98a255fcdc4a0db21a65791
Utkarsh Bhardwaj <ub2092000@gmail.com>
2024-04-08 17:47:44 +0530

    Fix 400 error on submitting question without image

--- ui/question-bank-ui/src/app/questions/questions.component.ts
+++ ui/question-bank-ui/src/app/questions/questions.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { QuestionsService } from '../services/questions.service';
 import { FormArray, FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { ChaptersService } from '../services/chapters.service';
@@ -89,49 +89,91 @@ export class QuestionsComponent implements OnInit {
   //  }
   // }
 
-  onSubmit(): void {
-    if (this.questionForm.valid) {
-       const formData = new FormData();
-       Object.keys(this.questionForm.value).forEach(key => {
-         formData.append(key, this.questionForm.value[key]);
-       });
-       // Append the file only if a file has been selected
-       if (this.selectedFile) {
-         formData.append('image', this.selectedFile, this.selectedFile.name);
-       }
+  // onSubmit(): void {
+  //   if (this.questionForm.valid) {
+  //      const formData = new FormData();
+  //      Object.keys(this.questionForm.value).forEach(key => {
+  //        formData.append(key, this.questionForm.value[key]);
+  //      });
+  //      // Append the file only if a file has been selected
+  //      if (this.selectedFile) {
+  //        formData.append('image', this.selectedFile, this.selectedFile.name);
+  //      }
    
-       this.questionsService.addQuestion(formData).subscribe(data => {
-         console.log('Question added:', data);
-         this.questions.push(data);
-         this.questionForm.reset();
-       }, error => {
-         console.error('Error adding question', error);
-       });
-    }
-   }
+  //      this.questionsService.addQuestion(formData).subscribe(data => {
+  //        console.log('Question added:', data);
+  //        this.questions.push(data);
+  //        this.questionForm.reset();
+  //      }, error => {
+  //        console.error('Error adding question', error);
+  //      });
+  //   }
+  //  }
 
 
 
   selectedFile: File | null = null;
 
-onFileSelected(event: any) {
- this.selectedFile = <File>event.target.files[0];
+onFileSelected(event: any): void {
+ if (event.target.files && event.target.files.length > 0) {
+    this.selectedFile = event.target.files[0];
+ } else {
+    this.selectedFile = null;
+ }
 }
 
-
-// deleteQuestion(questionId: number): void {
-//   if (confirm('Are you sure you want to delete this question?')) {
-//       this.questionsService.deleteQuestion(questionId).subscribe({
-//           next: response => {
-//               console.log(response);
-//               // Reload the questions list to ensure it's up-to-date
-//               this.loadQuestions();
-//           },
-//           error: error => {
-//               console.error(error);
-//           }
-//       });
-//   }
+// onFileSelected(event: any) {
+//  this.selectedFile = <File>event.target.files[0];
 // }
+
+
+deleteQuestion(questionId: number): void {
+  if (confirm('Are you sure you want to delete this question?')) {
+      this.questionsService.deleteQuestion(questionId).subscribe({
+          next: response => {
+              console.log(response);
+              // Reload the questions list to ensure it's up-to-date
+              this.loadQuestions();
+          },
+          error: error => {
+              console.error(error);
+          }
+      });
+  }
+}
+@ViewChild('fileInput') fileInput!: ElementRef;
+
+
+onSubmit(): void {
+  if (this.questionForm.valid) {
+     const formData = new FormData();
+     // Append each form field to the FormData object
+     Object.keys(this.questionForm.value).forEach(key => {
+       if (key !== 'image') { // Exclude the 'image' field from this loop
+         formData.append(key, this.questionForm.value[key]);
+       }
+     });
+ 
+     // Check if a file is selected for the image
+     if (this.selectedFile) {
+       // Append the selected file to the FormData object
+       formData.append('image', this.selectedFile, this.selectedFile.name);
+     }
+     // No need to explicitly append a null value for the image field if no file is selected
+ 
+     // Send the FormData object to the server
+     this.questionsService.addQuestion(formData).subscribe(data => {
+       console.log('Question added:', data);
+       this.questions.push(data);
+       this.questionForm.reset();
+       // Optionally, reset the selectedFile to null after successful submission
+       this.selectedFile = null;
+       this.fileInput.nativeElement.value = '';
+
+     }, error => {
+       console.error('Error adding question', error);
+     });
+  }
+ }
 
  }
