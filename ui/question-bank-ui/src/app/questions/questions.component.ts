e680d4b82eb7eb407edc14ee8512c05de8253dba
Utkarsh Bhardwaj <ub2092000@gmail.com>
2024-04-09 22:15:26 +0530

    Clean code

--- ui/question-bank-ui/src/app/questions/questions.component.ts
+++ ui/question-bank-ui/src/app/questions/questions.component.ts
@@ -7,8 +7,6 @@ import { SubjectsService } from '../services/subjects.service';
 import { StandardsService } from '../services/standards.service';
 import { CommonModule } from '@angular/common';
 import { QuestionDetailsComponent } from './questions-details/questions-details.component';
-// import { QuestionsGridComponent } from './questions-grid/questions-grid.component';
-// import { ApiService } from '../api.service';
 @Component({
   selector: 'app-questions',
   standalone: true,
@@ -22,7 +20,7 @@ export class QuestionsComponent implements OnInit {
   chapters: any[] = [];
   topics: any[] = [];
   subjects: any[] = [];
-  standards: any[] = []; // Array to hold the list of standards
+  standards: any[] = []; 
 
  
   constructor(private formBuilder: FormBuilder, private questionsService: QuestionsService, private chaptersService: ChaptersService, private topicsService: TopicsService, private subjectsService: SubjectsService, private standardsService: StandardsService) { 
@@ -31,12 +29,12 @@ export class QuestionsComponent implements OnInit {
       question_text: ['', Validators.required],
       type: ['', Validators.required],
       difficulty_level: ['', Validators.required],
-      standard: ['', Validators.required], // Use standard instead of class_level
+      standard: ['', Validators.required], 
       subject: ['', Validators.required],
       marks: ['', Validators.required],
       topic: ['', Validators.required],
       chapter: ['', Validators.required],
-      image: [null] // Optional image
+      image: [null] 
      });
   }
  
@@ -46,7 +44,7 @@ export class QuestionsComponent implements OnInit {
      this.loadChapters();
      this.loadTopics();
      this.loadSubjects();
-     this.loadStandards(); // Load standards when the component initializes
+     this.loadStandards(); 
 
   }
   loadStandards(): void {
@@ -78,41 +76,7 @@ export class QuestionsComponent implements OnInit {
      });
   }
  
-  // onSubmit(): void {
-  //   console.log(this.questionForm);
-  //    if (this.questionForm.valid) {
-  //      this.questionsService.addQuestion(this.questionForm.value).subscribe(data => {
-  //        console.log('Question added:', data);
-  //        this.questions.push(data);
-  //        this.questionForm.reset();
-  //      });
-  //    }
-  //    else {
-  //     console.log('Form is invalid');
-  //  }
-  // }
-
-  // onSubmit(): void {
-  //   if (this.questionForm.valid) {
-  //      const formData = new FormData();
-  //      Object.keys(this.questionForm.value).forEach(key => {
-  //        formData.append(key, this.questionForm.value[key]);
-  //      });
-  //      // Append the file only if a file has been selected
-  //      if (this.selectedFile) {
-  //        formData.append('image', this.selectedFile, this.selectedFile.name);
-  //      }
-   
-  //      this.questionsService.addQuestion(formData).subscribe(data => {
-  //        console.log('Question added:', data);
-  //        this.questions.push(data);
-  //        this.questionForm.reset();
-  //      }, error => {
-  //        console.error('Error adding question', error);
-  //      });
-  //   }
-  //  }
-
+  
 
 
   selectedFile: File | null = null;
@@ -125,17 +89,12 @@ onFileSelected(event: any): void {
  }
 }
 
-// onFileSelected(event: any) {
-//  this.selectedFile = <File>event.target.files[0];
-// }
-
 
 deleteQuestion(questionId: number): void {
   if (confirm('Are you sure you want to delete this question?')) {
       this.questionsService.deleteQuestion(questionId).subscribe({
           next: response => {
               console.log(response);
-              // Reload the questions list to ensure it's up-to-date
               this.loadQuestions();
           },
           error: error => {
@@ -150,32 +109,28 @@ deleteQuestion(questionId: number): void {
 onSubmit(): void {
   if (this.questionForm.valid) {
      const formData = new FormData();
-     // Append each form field to the FormData object
      Object.keys(this.questionForm.value).forEach(key => {
-       if (key !== 'image') { // Exclude the 'image' field from this loop
+       if (key !== 'image') { 
          formData.append(key, this.questionForm.value[key]);
        }
      });
  
-     // Check if a file is selected for the image
      if (this.selectedFile) {
-       // Append the selected file to the FormData object
        formData.append('image', this.selectedFile, this.selectedFile.name);
      }
-     // No need to explicitly append a null value for the image field if no file is selected
- 
-     // Send the FormData object to the server
-     this.questionsService.addQuestion(formData).subscribe(data => {
-       console.log('Question added:', data);
-       this.questions.push(data);
-       this.questionForm.reset();
-       // Optionally, reset the selectedFile to null after successful submission
-       this.selectedFile = null;
-       this.fileInput.nativeElement.value = '';
 
-     }, error => {
-       console.error('Error adding question', error);
-     });
+     this.questionsService.addQuestion(formData).subscribe({
+      next: data => {
+        console.log('Question added:', data);
+        this.questions.push(data);
+        this.questionForm.reset();
+        this.selectedFile = null;
+        this.fileInput.nativeElement.value = '';
+      },
+      error: error => {
+        console.error('Error adding question', error);
+      }
+    });
   }
  }
 
