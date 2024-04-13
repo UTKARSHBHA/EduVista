fa8d84910cef58a5eff836d493541181838c9536
mugambo <saurabhsharma24888@gmail.com>
2024-04-14 01:59:21 +0530

    implemented postgre

--- EduVistaServer/EduVistaServer/settings.py
+++ EduVistaServer/EduVistaServer/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'EduVistaServer.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'eduvista',
+        'USER': 'admin',
+        'PASSWORD': 'Chiku420',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
