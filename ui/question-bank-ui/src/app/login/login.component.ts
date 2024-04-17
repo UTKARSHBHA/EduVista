2eba7a913a40b94088eca0c5f85531a15334ec73
Utkarsh Bhardwaj <ub2092000@gmail.com>
2024-04-17 17:32:39 +0530

    Update login to redirect to home when verified

--- ui/question-bank-ui/src/app/login/login.component.ts
+++ ui/question-bank-ui/src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -11,32 +12,31 @@ import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angula
 })
 export class LoginComponent {
   loginForm: FormGroup;
-
- constructor(private formBuilder: FormBuilder, private authService: AuthService) {
-    this.loginForm = this.formBuilder.group({
-      username: ['', Validators.required],
-      password: ['', Validators.required]
-    });
- }
-
-
- loginUser() {
-  if (this.loginForm.valid) {
-    const userData = this.loginForm.value;
-    this.authService.login(userData).subscribe(
-      res => {
-        console.log(res);
-        this.authService.saveToken(res);
-        // Optionally, navigate to a protected route or dashboard here
-      },
-      err => {
-        console.error(err);
-        // Handle login errors, e.g., show an error message to the user
-      }
-    );
-  } else {
-    // Optionally, handle form validation errors
-    console.error('Form is invalid');
+ 
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { // Inject Router
+     this.loginForm = this.formBuilder.group({
+       username: ['', Validators.required],
+       password: ['', Validators.required]
+     });
   }
-}
-}
+ 
+  loginUser() {
+     if (this.loginForm.valid) {
+       const userData = this.loginForm.value;
+       this.authService.login(userData).subscribe({
+        next: (res) => {
+           console.log(res);
+           this.authService.saveToken(res);
+           this.router.navigate(['/']); // Navigate to home route
+        },
+        error: (err) => {
+           console.error(err);
+           // Handle login errors, e.g., show an error message to the user
+        }
+       });
+     } else {
+       // Optionally, handle form validation errors
+       console.error('Form is invalid');
+     }
+  }
+ }
