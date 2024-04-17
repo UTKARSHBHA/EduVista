1c2127ca1988f4ab4090d97d3ceea43069c35d5d
Utkarsh Bhardwaj <ub2092000@gmail.com>
2024-04-17 19:20:25 +0530

    Update login component

--- ui/question-bank-ui/src/app/login/login.component.ts
+++ ui/question-bank-ui/src/app/login/login.component.ts
@@ -1,19 +1,21 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule , CommonModule, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
   loginForm: FormGroup;
+  errorMessage: string = ''; // Store the error message
  
-  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) { // Inject Router
+  constructor(private formBuilder: FormBuilder, private authService: AuthService, private router: Router) {
      this.loginForm = this.formBuilder.group({
        username: ['', Validators.required],
        password: ['', Validators.required]
@@ -24,15 +26,17 @@ export class LoginComponent {
      if (this.loginForm.valid) {
        const userData = this.loginForm.value;
        this.authService.login(userData).subscribe({
-        next: (res) => {
+         next: (res) => {
            console.log(res);
            this.authService.saveToken(res);
            this.router.navigate(['/']); // Navigate to home route
-        },
-        error: (err) => {
+         },
+         error: (err) => {
            console.error(err);
            // Handle login errors, e.g., show an error message to the user
-        }
+           this.errorMessage = 'Invalid username or password. Please try again.';
+           this.loginForm.reset();
+         }
        });
      } else {
        // Optionally, handle form validation errors
