cddd2d275af261a02491631abeac7dc1d1d9b8fb
Utkarsh Bhardwaj <ub2092000@gmail.com>
2024-04-17 17:44:59 +0530

    Add guard to all routes except home

--- ui/question-bank-ui/src/app/app.routes.ts
+++ ui/question-bank-ui/src/app/app.routes.ts
@@ -6,22 +6,16 @@ import { QuestionsComponent } from './questions/questions.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
-    {path: 'standards',loadComponent: () => import("./standards/standards.component").then((m) => m.StandardsComponent) },
-    {path: 'subjects',loadComponent: () => import("./subjects/subjects.component").then((m) => m.SubjectsComponent) },
-    {path: 'chapters',loadComponent: () => import("./chapters/chapters.component").then((m) => m.ChaptersComponent) },
-    {path: 'topics',loadComponent: () => import("./topics/topics.component").then((m) => m.TopicsComponent) },
-    {path: 'questions',loadComponent: () => import("./questions/questions.component").then((m) => m.QuestionsComponent) },
-    {path: 'options',loadComponent: () => import("./options/options.component").then((m) => m.OptionsComponent) },
-    {path: 'question-generator',loadComponent: () => import("./question-paper-generator/question-paper-generator.component").then((m) => m.QuestionPaperGeneratorComponent) },
-
-    {path: 'questions-list',loadComponent: () => import("./questions/questions-list/questions-list.component").then((m) => m.QuestionsListComponent) },
-    {path: 'questions-details',loadComponent: () => import("./questions/questions-details/questions-details.component").then((m) => m.QuestionDetailsComponent) },
-    {path: 'question-view/:id',loadComponent: () => import("./questions/question-view/question-view.component").then((m) => m.QuestionViewComponent) },
-
-    { path: 'questions/:id', loadComponent: () => import("./questions/questions.component").then((m) => m.QuestionsComponent) },
-    { path: 'login', loadComponent: () => import("./login/login.component").then((m) => m.LoginComponent) },
-
-
-    // { path: 'protected', component: QuestionsComponent, canActivate: [AuthGuard] },
-
+    {path: 'standards', loadComponent: () => import("./standards/standards.component").then((m) => m.StandardsComponent), canActivate: [AuthGuard]},
+    {path: 'subjects', loadComponent: () => import("./subjects/subjects.component").then((m) => m.SubjectsComponent), canActivate: [AuthGuard]},
+    {path: 'chapters', loadComponent: () => import("./chapters/chapters.component").then((m) => m.ChaptersComponent), canActivate: [AuthGuard]},
+    {path: 'topics', loadComponent: () => import("./topics/topics.component").then((m) => m.TopicsComponent), canActivate: [AuthGuard]},
+    {path: 'questions', loadComponent: () => import("./questions/questions.component").then((m) => m.QuestionsComponent), canActivate: [AuthGuard]},
+    {path: 'options', loadComponent: () => import("./options/options.component").then((m) => m.OptionsComponent), canActivate: [AuthGuard]},
+    {path: 'question-generator', loadComponent: () => import("./question-paper-generator/question-paper-generator.component").then((m) => m.QuestionPaperGeneratorComponent), canActivate: [AuthGuard]},
+    {path: 'questions-list', loadComponent: () => import("./questions/questions-list/questions-list.component").then((m) => m.QuestionsListComponent), canActivate: [AuthGuard]},
+    {path: 'questions-details', loadComponent: () => import("./questions/questions-details/questions-details.component").then((m) => m.QuestionDetailsComponent), canActivate: [AuthGuard]},
+    {path: 'question-view/:id', loadComponent: () => import("./questions/question-view/question-view.component").then((m) => m.QuestionViewComponent), canActivate: [AuthGuard]},
+    {path: 'questions/:id', loadComponent: () => import("./questions/questions.component").then((m) => m.QuestionsComponent), canActivate: [AuthGuard]},
+    {path: 'login', loadComponent: () => import("./login/login.component").then((m) => m.LoginComponent)},
 ];
--- ui/question-bank-ui/src/app/auth.guard.ts
+++ ui/question-bank-ui/src/app/auth.guard.ts
@@ -1,24 +1,19 @@
-// auth.guard.ts
-
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+// Import necessary modules
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
 import { Observable } from 'rxjs';
+import { inject } from '@angular/core';
 
-@Injectable({
- providedIn: 'root'
-})
-export class AuthGuard implements CanActivate {
- constructor(private router: Router) { }
-
- canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    const token = localStorage.getItem('access_token');
-    if (token) {
-      return true;
-    } else {
-      this.router.navigate(['/login']);
-      return false;
-    }
+// Define the functional guard
+export function AuthGuard(
+ route: ActivatedRouteSnapshot,
+ state: RouterStateSnapshot
+): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+ const token = localStorage.getItem('access_token');
+ const router = inject(Router);
+ if (token) {
+    return true;
+ } else {
+    router.navigate(['/login']); // Redirect to login page
+    return false;
  }
 }
