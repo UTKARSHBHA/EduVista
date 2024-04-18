994caf521ba7300cdf85f5506899cdbddb343986
Utkarsh Bhardwaj <ub2092000@gmail.com>
2024-04-18 11:08:20 +0530

    Add nav-bar

--- ui/question-bank-ui/src/app/app.component.ts
+++ ui/question-bank-ui/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { StandardsComponent } from './standards/standards.component';
 import { SubjectsComponent } from './subjects/subjects.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet , StandardsComponent, SubjectsComponent , RouterModule],
+  imports: [RouterOutlet , StandardsComponent, SubjectsComponent , RouterModule, NavBarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ ui/question-bank-ui/src/app/nav-bar/nav-bar.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.css'
+})
+export class NavBarComponent {
+  constructor(public authService: AuthService , private router: Router) {}
+ 
+  // isLoggedIn(): boolean {
+  //    return this.authService.isLoggedIn(); // Implement this method in your AuthService
+  // }
+ 
+  login() {
+     // Implement your login logic here
+     this.router.navigate(['/login']);
+
+  }
+ 
+  logout() {
+     // Implement your logout logic here
+     this.authService.logout();
+     this.router.navigate(['/']);
+
+  }
+ }
+ 
