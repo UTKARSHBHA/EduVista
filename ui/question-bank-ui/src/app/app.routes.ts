0f3220346fab3a96b9a810b098347028338f58de
Utkarsh Bhardwaj <ub2092000@gmail.com>
2024-04-23 14:01:29 +0530

    Add ui for question-paper-generator

--- ui/question-bank-ui/src/app/app.routes.ts
+++ ui/question-bank-ui/src/app/app.routes.ts
@@ -5,24 +5,117 @@ import { AuthGuard } from './auth.guard';
 import { QuestionsComponent } from './questions/questions.component';
 
 export const routes: Routes = [
-    {path: '', component: HomeComponent},
-    {path: 'standards', loadComponent: () => import("./standards/standards.component").then((m) => m.StandardsComponent), canActivate: [AuthGuard]},
-    {path: 'subjects', loadComponent: () => import("./subjects/subjects.component").then((m) => m.SubjectsComponent), canActivate: [AuthGuard]},
-    {path: 'chapters', loadComponent: () => import("./chapters/chapters.component").then((m) => m.ChaptersComponent), canActivate: [AuthGuard]},
-    {path: 'topics', loadComponent: () => import("./topics/topics.component").then((m) => m.TopicsComponent), canActivate: [AuthGuard]},
-    {path: 'questions', loadComponent: () => import("./questions/questions.component").then((m) => m.QuestionsComponent), canActivate: [AuthGuard]},
-    {path: 'options', loadComponent: () => import("./options/options.component").then((m) => m.OptionsComponent), canActivate: [AuthGuard]},
-    {path: 'question-generator', loadComponent: () => import("./question-paper-generator/question-paper-generator.component").then((m) => m.QuestionPaperGeneratorComponent), canActivate: [AuthGuard]},
-    {path: 'questions-list', loadComponent: () => import("./questions/questions-list/questions-list.component").then((m) => m.QuestionsListComponent), canActivate: [AuthGuard]},
-    {path: 'questions-details', loadComponent: () => import("./questions/questions-details/questions-details.component").then((m) => m.QuestionDetailsComponent), canActivate: [AuthGuard]},
-    {path: 'question-view/:id', loadComponent: () => import("./questions/question-view/question-view.component").then((m) => m.QuestionViewComponent), canActivate: [AuthGuard]},
-    {path: 'questions/:id', loadComponent: () => import("./questions/questions.component").then((m) => m.QuestionsComponent), canActivate: [AuthGuard]},
-    {path: 'login', loadComponent: () => import("./login/login.component").then((m) => m.LoginComponent)},
-    {path: 'signup', loadComponent: () => import("./signup/signup.component").then((m) => m.SignupComponent)},
-    {path: 'password-reset', loadComponent: () => import("./password-reset/password-reset.component").then((m) => m.PasswordResetComponent)},
-    {path: 'api/password_reset_confirm/:token', loadComponent: () => import("./password-reset-confirm/password-reset-confirm.component").then((m) => m.PasswordResetConfirmComponent)},
-    {path: 'change-password', loadComponent: () => import("./change-password/change-password.component").then((m) => m.ChangePasswordComponent), canActivate: [AuthGuard]},
-    
-    
-
+  { path: '', component: HomeComponent },
+  {
+    path: 'standards',
+    loadComponent: () =>
+      import('./standards/standards.component').then(
+        (m) => m.StandardsComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'subjects',
+    loadComponent: () =>
+      import('./subjects/subjects.component').then((m) => m.SubjectsComponent),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'chapters',
+    loadComponent: () =>
+      import('./chapters/chapters.component').then((m) => m.ChaptersComponent),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'topics',
+    loadComponent: () =>
+      import('./topics/topics.component').then((m) => m.TopicsComponent),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'questions',
+    loadComponent: () =>
+      import('./questions/questions.component').then(
+        (m) => m.QuestionsComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'options',
+    loadComponent: () =>
+      import('./options/options.component').then((m) => m.OptionsComponent),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'question-generator',
+    loadComponent: () =>
+      import(
+        './question-paper-generator/question-paper-generator.component'
+      ).then((m) => m.QuestionPaperGeneratorComponent),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'questions-list',
+    loadComponent: () =>
+      import('./questions/questions-list/questions-list.component').then(
+        (m) => m.QuestionsListComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'questions-details',
+    loadComponent: () =>
+      import('./questions/questions-details/questions-details.component').then(
+        (m) => m.QuestionDetailsComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'question-view/:id',
+    loadComponent: () =>
+      import('./questions/question-view/question-view.component').then(
+        (m) => m.QuestionViewComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'questions/:id',
+    loadComponent: () =>
+      import('./questions/questions.component').then(
+        (m) => m.QuestionsComponent
+      ),
+    canActivate: [AuthGuard],
+  },
+  {
+    path: 'login',
+    loadComponent: () =>
+      import('./login/login.component').then((m) => m.LoginComponent),
+  },
+  {
+    path: 'signup',
+    loadComponent: () =>
+      import('./signup/signup.component').then((m) => m.SignupComponent),
+  },
+  {
+    path: 'password-reset',
+    loadComponent: () =>
+      import('./password-reset/password-reset.component').then(
+        (m) => m.PasswordResetComponent
+      ),
+  },
+  {
+    path: 'api/password_reset_confirm/:token',
+    loadComponent: () =>
+      import('./password-reset-confirm/password-reset-confirm.component').then(
+        (m) => m.PasswordResetConfirmComponent
+      ),
+  },
+  {
+    path: 'change-password',
+    loadComponent: () =>
+      import('./change-password/change-password.component').then(
+        (m) => m.ChangePasswordComponent
+      ),
+    canActivate: [AuthGuard],
+  },
 ];
--- ui/question-bank-ui/src/app/question-paper-generator/question-paper-generator.component.ts
+++ ui/question-bank-ui/src/app/question-paper-generator/question-paper-generator.component.ts
@@ -1,8 +1,12 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { QuestionsService } from '../services/questions.service';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { StandardsService } from '../services/standards.service';
+import { SubjectsService } from '../services/subjects.service';
+import { TopicsService } from '../services/topics.service';
+import { ChaptersService } from '../services/chapters.service';
 
 @Component({
   selector: 'app-question-paper-generator',
@@ -11,44 +15,130 @@ import { CommonModule } from '@angular/common';
   templateUrl: './question-paper-generator.component.html',
   styleUrl: './question-paper-generator.component.css'
 })
-export class QuestionPaperGeneratorComponent implements OnInit {
+export class QuestionPaperGeneratorComponent {
   questionPaperForm: FormGroup;
-  questionTypes = ['mcq', 'tf', 'descriptive'];
-  difficultyLevels = ['easy', 'medium', 'hard'];
   questionPaper: any[] = [];
  
-  constructor(private formBuilder: FormBuilder, private questionsService: QuestionsService) {
-     this.questionPaperForm = this.formBuilder.group({});
-     this.initializeFormControls();
-  }
+  
+  standards: any[] = [];
+  subjects: any[] = [];
+  topics: any[] = [];
+  chapters: any[] = [];
+  selectedStandard: any = null;
+  selectedSubject: any = null;
+  selectedTopics: any[] = [];
+  selectedChapters: any[] = [];
+
  
-  ngOnInit(): void {}
- 
-  initializeFormControls(): void {
-     this.questionTypes.forEach(type => {
-       this.questionPaperForm.addControl(type, new FormControl(false));
-       this.questionPaperForm.addControl(`${type}Quantity`, new FormControl(null, Validators.min(0)));
+  constructor(
+    private formBuilder: FormBuilder,
+  ) {
+     this.questionPaperForm = this.formBuilder.group({
+       standard: ['', Validators.required],
+       subject: ['', Validators.required],
+       chapters: this.formBuilder.array([]),
+       topics: this.formBuilder.array([]),
+       easy: [0],
+       medium: [0],
+       hard: [0]
      });
-     this.difficultyLevels.forEach(level => {
-       this.questionPaperForm.addControl(level, new FormControl(false));
-       this.questionPaperForm.addControl(`${level}Quantity`, new FormControl(null, Validators.min(0)));
-     });
+     this.loadStandards();
   }
+  
+  ngOnInit() {
+    // Initialize chapters and topics FormArrays
+    this.initChaptersFormArray();
+    this.initTopicsFormArray();
+ }
+
+ initChaptersFormArray() {
+    const chaptersArray = this.questionPaperForm.get('chapters') as FormArray;
+    this.chapters.forEach(chapter => {
+      chaptersArray.push(new FormControl(false)); // Initialize each chapter checkbox as unchecked
+    });
+ }
+
+ initTopicsFormArray() {
+    const topicsArray = this.questionPaperForm.get('topics') as FormArray;
+    this.topics.forEach(topic => {
+      topicsArray.push(new FormControl(false)); // Initialize each topic checkbox as unchecked
+    });
+ }
+
+  loadStandards(): void {
+    // this.standardsService.getStandards().subscribe(data => {
+    //   this.standards = data;
+    // });
+    this.standards = [
+      { id: 1, name: 'Standard 1' },
+    { id: 2, name: 'Standard 2' },
+    ];
+ }
+
+ onStandardSelected(e: any): void {
+    // this.subjectsService.getSubjectsByStandard(standardId).subscribe(data => {
+    //   this.subjects = data;
+    //   this.selectedStandard = standardId;
+    // });
+    console.log(e.target.value);
+    this.selectedStandard = e.target.value;
+    this.subjects = [
+      { id: 1, name: 'Subject 1' },
+    { id: 2, name: 'Subject 2' },
+    ];
+
+  }
+
+ onSubjectSelected(e: any): void {
+    // this.topicsService.getTopicsBySubject(subjectId).subscribe(data => {
+    //   this.topics = data;
+    //   this.selectedSubject = subjectId;
+    // });
+    console.log(e.target.value);
+    this.selectedSubject   = e.target.value;
+    this.chapters = [
+      { id: 1, name: 'Chapter 1' },
+      { id: 2, name: 'Chapter 2' },
+    ];
+   }
+   onChapterSelected(event: any, topicId: number): void {
+    if (event.target.checked) {
+        // If the checkbox is checked, add the topic ID to the selectedTopics array
+        this.selectedChapters.push(topicId);
+        console.log(this.selectedChapters);
+        this.topics = [
+          { id: 1, name: 'Topic 1' },
+          { id: 2, name: 'Topic 2' },
+        ];
+    } else {
+        // If the checkbox is unchecked, remove the topic ID from the selectedChapters array
+        const index = this.selectedChapters.indexOf(topicId);
+        if (index > -1) {
+            this.selectedChapters.splice(index, 1);
+        }
+    }
+    // Optionally, you can update a form control with the selected topics here
+  }
+
+ onTopicSelected(event: any, topicId: number): void {
+  if (event.target.checked) {
+      // If the checkbox is checked, add the topic ID to the selectedTopics array
+      this.selectedTopics.push(topicId);
+  } else {
+      // If the checkbox is unchecked, remove the topic ID from the selectedTopics array
+      const index = this.selectedTopics.indexOf(topicId);
+      if (index > -1) {
+          this.selectedTopics.splice(index, 1);
+      }
+  }
+  // Optionally, you can update a form control with the selected topics here
+}
  
   generateQuestionPaper(): void {
     //  if (this.questionPaperForm.valid) {
-    //    const selectedTypes = this.questionTypes.filter(type => this.questionPaperForm.get(type).value);
-    //    const selectedLevels = this.difficultyLevels.filter(level => this.questionPaperForm.get(level).value);
-    //    const quantities = {};
-    //    selectedTypes.forEach(type => {
-    //      quantities[type] = this.questionPaperForm.get(`${type}Quantity`).value;
-    //    });
-    //    selectedLevels.forEach(level => {
-    //      quantities[level] = this.questionPaperForm.get(`${level}Quantity`).value;
-    //    });
-    //    this.questionsService.generateQuestionPaper(quantities).subscribe(questions => {
-    //      this.questionPaper = questions;
-    //    });
+       const selectedCriteria = this.questionPaperForm.value;
+       console.log(selectedCriteria);
+      
     //  }
   }
  }
