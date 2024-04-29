85f08fb6380dc6fdd319ada8e96fbff79703b5c9
Utkarsh Bhardwaj <ub2092000@gmail.com>
2024-04-29 18:02:21 +0530

    Update standards to use ng select and include subjects field

--- ui/question-bank-ui/src/app/standards/standards.component.ts
+++ ui/question-bank-ui/src/app/standards/standards.component.ts
@@ -3,69 +3,57 @@ import { StandardsService } from '../services/standards.service';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatDialogRef } from '@angular/material/dialog';
 import { MatDialogModule } from '@angular/material/dialog';
+import { SubjectsService } from '../services/subjects.service';
+import { NgSelectModule } from '@ng-select/ng-select'; 
+
 @Component({
   selector: 'app-standards',
   standalone: true,
-  imports: [ReactiveFormsModule ,    MatDialogModule
+  imports: [ReactiveFormsModule ,    MatDialogModule, NgSelectModule
   ],
   templateUrl: './standards.component.html',
   styleUrl: './standards.component.css',
 })
 export class StandardsComponent {
-//   CloseDialog() {
-//     if (this._mdr) {
-//         this._mdr.close(false);
-//     }
-// }
-  standards: any = [];
-  standardForm: FormGroup;
+  standardsForm: FormGroup;
+  subjects: any[] = [];
 
-
-  constructor(private formBuilder: FormBuilder, private standardsService: StandardsService) { 
-    this.standardForm = this.formBuilder.group({
-      name: ['', Validators.required]
-   });
-
-  }
+ constructor(
+    private formBuilder: FormBuilder,
+    private standardsService: StandardsService,
+    private subjectsService: SubjectsService
+ ) {
+    this.standardsForm = this.formBuilder.group({
+      name: ['', Validators.required],
+      subjects: [[]],
+    });
+ }
 
  ngOnInit(): void {
-    // this.service.getStandards().subscribe(data => {
-    //   this.standards = data;
-    //   console.log(this.standards);
-    // });
-
-    // this.standardForm = this.formBuilder.group({
-    //   name: ['', Validators.required]
-    // });
-
-    // this.loadStandards();
+    this.loadSubjects();
  }
- loadStandards(): void {
-  this.standardsService.getStandards().subscribe(data => {
-    this.standards = data;
-  });
-}
-onSubmit(): void {
-  if (this.standardForm.valid) {
-    this.standardsService.addStandard(this.standardForm.value).subscribe(data => {
-      console.log('Standard added:', data);
-      this.standards.push(data);
-      this.standardForm.reset();
+
+ loadSubjects(): void {
+    this.subjectsService.getSubjects().subscribe(data => {
+      this.subjects = data;
     });
-  }
-}
-deleteStandard(standardId: number): void {
-  if (confirm('Are you sure you want to delete this standard?')) {
-      this.standardsService.deleteStandard(standardId).subscribe({
-          next: response => {
-              console.log(response);
-              // Reload the standards list to ensure it's up-to-date
-              this.loadStandards();
-          },
-          error: error => {
-              console.error(error);
-          }
-      });
+ }
+
+ onSubmit(): void {
+  if (this.standardsForm.valid) { 
+    const formValue = this.standardsForm.value;
+    
+    this.standardsService.addStandard(formValue).subscribe(
+      (response) => {
+        console.log('Standard added:', response);
+        this.standardsForm.reset(); // Reset the form
+      },
+      (error) => {
+        console.error('Error:', error);
+      }
+    );
+  } else {
+    console.error('Form is invalid');
   }
 }
 }
