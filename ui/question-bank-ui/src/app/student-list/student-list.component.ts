c8159fba9c2918f6e0edd256c08dca917bb4e0cf
Utkarsh Bhardwaj <ub2092000@gmail.com>
2024-05-16 13:03:28 +0530

    Add student details column to display multiple data fields

--- ui/question-bank-ui/src/app/student-list/student-list.component.ts
+++ ui/question-bank-ui/src/app/student-list/student-list.component.ts
@@ -19,24 +19,49 @@ export class StudentListComponent implements OnInit {
   genderRenderer = (params:any) => {
     switch (params.value) {
       case 'M':
-        return 'Male';
+        return '<i class="fa-solid fa-mars" style="color: blue"></i>';
       case 'F':
-        return 'Female';
+        return '<i class="fa-solid fa-venus" style="color: pink"></i>';
       case 'O':
-        return 'Others';
+        return '<i class="fa-solid fa-venus-mars"></i>';
       default:
         return 'Unknown';
     }
   };
+  detailsRenderer = (params:any) => {
+    const data = params.data;
+    return `
+      <div>
+        <div>${this.genderRenderer({ value: data.gender })}
+        ${data.first_name} ${data.last_name}</div>
+        <div><i class="fa-solid fa-envelope"></i> ${data.user?.email}</div>
+        <div><i class="fa-solid fa-phone"></i> ${data.phone_number}</div>
+      </div>
+    `;
+  };
 
   colDefs = [
-    { headerName: 'First Name', field: 'first_name', filter: true },
-    { headerName: 'Last Name', field: 'last_name', filter: true },
-    { headerName: 'Email', field: 'user.email', filter: true },
+    {
+      headerName: 'Student Details',
+      field: 'details',
+      cellRenderer: this.detailsRenderer,
+      autoHeight: true,
+      valueGetter: (params: any) => {
+        const data = params.data;
+        return `${data.gender} ${data.first_name} ${data.last_name} ${data.user?.email} ${data.phone_number}`;
+      },
+      filter: 'agTextColumnFilter',
+
+
+    },
+    
+    { headerName: 'First Name', field: 'first_name', filter: true, hide: true },
+    { headerName: 'Last Name', field: 'last_name', filter: true, hide: true  },
+    { headerName: 'Email', field: 'user.email', filter: true, hide: true  },
+    { headerName: 'Gender', field: 'gender', cellRenderer: this.genderRenderer, filter: true, hide: true  },
+    { headerName: 'Phone Number', field: 'phone_number', filter: true, hide: true  },
     { headerName: 'Username', field: 'user.username', filter: true },
-    { headerName: 'Phone Number', field: 'phone_number', filter: true },
     { headerName: 'Date of Birth', field: 'date_of_birth', filter: true },
-    { headerName: 'Gender', field: 'gender', cellRenderer: this.genderRenderer, filter: true },
     { headerName: 'Registration Number', field: 'registration_number', filter: true },
     { headerName: 'Admission Date', field: 'admission_date', filter: true },
     { headerName: 'Address', field: 'address_line1', filter: true },
@@ -45,7 +70,6 @@ export class StudentListComponent implements OnInit {
     { headerName: 'State', field: 'state', filter: true },
     { headerName: 'Postal Code', field: 'postal_code', filter: true },
     { headerName: 'Country', field: 'country', filter: true },
-    // { headerName: 'Profile Picture', field: 'profile_picture', cellRenderer: 'agGridImageRenderer', filter: true },
     { headerName: 'Parent/Guardian Name', field: 'parent_guardian_name', filter: true },
     { headerName: 'Parent/Guardian Contact', field: 'parent_guardian_contact', filter: true },
     { headerName: 'Emergency Contact Name', field: 'emergency_contact_name', filter: true },
@@ -87,4 +111,6 @@ export class StudentListComponent implements OnInit {
       disableClose: true,
     });
   }
+
+ 
 };
